dbb73773cfd960c8993a99f69982180a6361d28c
noracami <kerke.2011@gmail.com>
2024-02-25 01:58:29 +0800

    🛂 allow iframe

--- app/controllers/frontend/games_controller.rb
+++ app/controllers/frontend/games_controller.rb
@@ -1,8 +1,15 @@
 module Frontend
   class GamesController < ApplicationController
+    after_filter :allow_iframe, only: %i(show)
+
     def show
       Rails.logger.warn { "Frontend::GamesController#show #{params[:id]}" }
       render layout: false
+    end
+
+    private
+    def allow_iframe
+      response.headers.delete "X-Frame-Options"
     end
   end
 end
