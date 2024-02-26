19f83baa28bcb5cd908d07fea4ce2e7bd2ff677f
noracami <kerke.2011@gmail.com>
2024-02-27 00:19:02 +0800

    🐛 key typo

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -3,7 +3,7 @@ module Admin
     def create
       room_id = "dev_#{SecureRandom.base36(13)}"
       players = [{id: SecureRandom.base36(24), nickname: 'Me'}]
-      3.times { players << {id: SecureRandom.base36(24), nickname: Faker::Name.first_name} }
+      3.times { players << {id: SecureRandom.base36(24), nickName: Faker::Name.first_name} }
 
       game_id = SecureRandom.base36(8)
       game_id = SecureRandom.base36(8) while Game.exists?(uuid: game_id)
