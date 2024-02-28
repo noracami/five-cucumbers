9ab15196e3b00b1ce98cdff8b158f186eca58b2f
noracami <kerke.2011@gmail.com>
2024-02-29 04:54:24 +0800

    👷 apply cookie in iframe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,8 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # In order for your session cookie to work inside of an iframe, you need to explicitly set its SameSite setting to None.
+  # https://stackoverflow.com/questions/69652408/sessions-of-rails-app-loaded-in-iframe-no-working
+  config.action_dispatch.cookies_same_site_protection = :none
 end
