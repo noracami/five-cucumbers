a0505fbd00dcb17bf72bcd5a23067691ab6b8804
noracami <kerke.2011@gmail.com>
2024-03-23 02:29:36 +0800

    🎨 modify ui, hide uuid in frontend

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -2,4 +2,8 @@ module GamesHelper
   class ::Integer
     def to_card_number = Games::Card.new(id: self).numbers
   end
+
+  def shorten_uuid(uuid)
+    Digest::MD5.hexdigest(uuid).to_i(16).modulo(9999).to_s.rjust(4, '0').prepend('#')
+  end
 end
